ae740c2e7af44c0e29508aa021667b724e4c3bef
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-08-22 16:41:07 +0700

    Perbaikan Bug Final

--- static/js/presensi/cihuyred.js
+++ static/js/presensi/cihuyred.js
@@ -1,4 +1,4 @@
-// import { CihuyTokRed} from "https://c-craftjs.github.io/link/link.js"
+import { CihuyTokRed} from "https://c-craftjs.github.io/link/link.js"
 
-// const redirecturl = "euis.ulbi.ac.id"
-// CihuyTokRed(redirecturl);
+const redirecturl = "euis.ulbi.ac.id"
+CihuyTokRed(redirecturl);

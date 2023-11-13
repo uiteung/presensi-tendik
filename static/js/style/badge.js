e6073b889f604e8190310fce25369264ebd21c83
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-13 11:37:17 +0700

    add(badge) : create function badge detail

--- static/js/style/badge.js
+++ static/js/style/badge.js
@@ -26,3 +26,32 @@ export function getStatusBadgeMarkup(persentaseStatus) {
       return '';
     }
 }
+
+// Function to get badge markup based on the status
+export function getBadgeMarkupDetail(status) {
+    switch (status) {
+        case 'Lebih Cepat':
+            return '<span class="badge-green" style="font-size: 10px; background-color: #22bb33; color: white; padding: 5px 10px; border-radius: 5px;">Lebih Cepat</span>';
+        case 'Tepat Waktu':
+            return '<span class="badge-blue" style="font-size: 10px; background-color: #28a745; color: white; padding: 5px 10px; border-radius: 5px;">Tepat Waktu</span>';
+        case 'Terlambat':
+            return '<span class="badge-danger" style="font-size: 10px; background-color: #bb2124; color: white; padding: 5px 10px; border-radius: 5px;">Terlambat</span>';
+        case 'Sakit':
+            return '<span class="badge-warning" style="font-size: 10px; background-color: #ffcc00; color: white; padding: 5px 10px; border-radius: 5px;">Sakit</span>';
+        case 'izin':
+            return '<span class="badge-warning" style="font-size: 10px; background-color: #ff8700; color: white; padding: 5px 10px; border-radius: 5px;">Izin</span>';
+        default:
+            return "<span>Belum Presensi</span>";
+    }
+}
+
+// Function to get badge markup based on the status
+export function getStatusBadgeMarkupDetail(persentaseStatus) {
+    if (persentaseStatus >= 100) {
+        return '<span class="badge-danger" style="font-size: 10px; background-color: #22bb33; color: white; padding: 5px 10px; border-radius: 5px;">Tuntas</span>';
+    } else if (persentaseStatus < 100) {
+        return '<span class="badge-danger" style="font-size: 10px; background-color: #bb2124; color: white; padding: 5px 10px; border-radius: 5px;">Belum Tuntas</span>';
+    } else {
+        return '';
+    }
+}

5604f18362064551b396aff439b6a7fea9bf4b32
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-13 09:55:05 +0700

    fix(pagination) : fix bug pagination

--- static/js/presensi/index.js
+++ static/js/presensi/index.js
@@ -25,18 +25,6 @@ export_button.addEventListener('click', () => {
   html_table_to_excel('xlsx');
 })
 
-// // Function to export data to PDF
-// function html_table_to_pdf() {
-//   const doc = new jsPDF();
-//   doc.autoTable({ html: '#example' });
-//   doc.save('Data_Rekap_Presensi.pdf');
-// }
-// const exportPdfBtn = document.getElementById('exportPdfBtn');
-// exportPdfBtn.addEventListener('click', () => {
-//   html_table_to_pdf();
-// });
-
-// Untuk Membuat Pagination
 CihuyDomReady(() => {
   const tablebody = CihuyId("tablebody");
   const buttonsebelumnya = CihuyId("prevPageBtn");
@@ -44,177 +32,195 @@ CihuyDomReady(() => {
   const halamansaatini = CihuyId("currentPage");
   const itemperpage = 8;
   let halamannow = 1;
+  let filteredData = []; // To store the filtered data for search
+  let totalData = 0;
 
   // Ambil data masuk
   fetch("https://hris_backend.ulbi.ac.id/presensi/datapresensi", requestOptions)
-  .then((result) => result.json())
-  .then((masukData) => {
-    // Ambil data pulang
-    fetch("https://hris_backend.ulbi.ac.id/presensi/datapresensi/pulang", requestOptions)
     .then((result) => result.json())
-    .then((pulangData) => {
-      // Gabungkan data berdasarkan nama dan tanggal yang sesuai
-      const combinedData = masukData.data.map((masukEntry) => {
-        const matchingPulangEntry = pulangData.data.find((pulangEntry) =>
-          pulangEntry.biodata.nama === masukEntry.biodata.nama &&
-          new Date(pulangEntry.datetime).toLocaleDateString() === new Date(masukEntry.Datetime).toLocaleDateString()
-        );
+    .then((masukData) => {
+      // Ambil data pulang
+      fetch("https://hris_backend.ulbi.ac.id/presensi/datapresensi/pulang", requestOptions)
+        .then((result) => result.json())
+        .then((pulangData) => {
+          // Gabungkan data berdasarkan nama dan tanggal yang sesuai
+          const combinedData = masukData.data.map((masukEntry) => {
+            const matchingPulangEntry = pulangData.data.find((pulangEntry) =>
+              pulangEntry.biodata.nama === masukEntry.biodata.nama &&
+              new Date(pulangEntry.datetime).toLocaleDateString() === new Date(masukEntry.Datetime).toLocaleDateString()
+            );
 
-        return {
-          masuk: masukEntry,
-          pulang: matchingPulangEntry,
-        };
-      });
+            return {
+              masuk: masukEntry,
+              pulang: matchingPulangEntry,
+            };
+          });
 
-      // Sortir array combinedData berdasarkan tanggal masuk
-      combinedData.sort((a, b) => new Date(b.masuk.Datetime).getTime() - new Date(a.masuk.Datetime).getTime());
+          // Sortir array combinedData berdasarkan tanggal masuk
+          combinedData.sort((a, b) => new Date(b.masuk.Datetime).getTime() - new Date(a.masuk.Datetime).getTime());
 
-      // Inisialisasi data tabel
-      let tableData = "";
+          // Inisialisasi data tabel
+          totalData = combinedData.length;
+          filteredData = combinedData;
+          
+          // Untuk Memunculkan Pagination Halamannya
+          displayData(halamannow);
+          updatePagination();
 
-      // Iterasi melalui combinedData dan membangun baris tabel
-      combinedData.forEach((combinedEntry) => {
-        const masukEntry = combinedEntry.masuk;
-        const pulangEntry = combinedEntry.pulang;
+          // Button Pagination (Sebelumnya)
+          buttonsebelumnya.addEventListener("click", () => {
+            if (halamannow > 1) {
+              halamannow--;
+              displayData(halamannow);
+              updatePagination();
+            }
+          });
 
-        // Ekstrak data yang relevan
-        const nama = masukEntry.biodata.nama;
-        const lampiran = masukEntry.lampiran;
-        const ketMasuk = masukEntry?.ket;
-        const ketPulang = pulangEntry?.ket;
-        const date = new Date(masukEntry.Datetime).toLocaleDateString();
-        const jamMasuk = new Date(masukEntry.Datetime).toLocaleTimeString();
-        const jamPulang = pulangEntry ? new Date(pulangEntry.datetime).toLocaleTimeString() : '';
-        const Durasi = pulangEntry ? pulangEntry.durasi : '0 Jam 0 Menit 0 Detik';
-        const persentaseStatus = pulangEntry ? parseFloat(pulangEntry.persentase) : 0;
-        const Persentase = pulangEntry ? pulangEntry.persentase : '0%';
-        const lampiranContent = lampiran ? lampiran : '<p>Tidak Ada Catatan</p>';
+          // Button Pagination (Selanjutnya)
+          buttonselanjutnya.addEventListener("click", () => {
+            const totalPages = Math.ceil(totalData / itemperpage);
+            if (halamannow < totalPages) {
+              halamannow++;
+              displayData(halamannow);
+              updatePagination();
+            }
+          });
 
-          // Pengkondisian Badge Keterangan
-          let ketBadgeMasuk = '';
-          if (ketMasuk === 'Lebih Cepat') {
-            ketBadgeMasuk = '<span class="badge-green" style="font-size: 10px; background-color: #22bb33; color: white; padding: 5px 10px; border-radius: 5px;">Masuk Lebih Cepat</span>';
-          } else if (ketMasuk === 'Tepat Waktu') {
-            ketBadgeMasuk = '<span class="badge-blue" style="font-size: 10px; background-color: #0d6efd; color: white; padding: 5px 10px; border-radius: 5px;">Masuk Tepat Waktu</span>';
-          } else if (ketMasuk === 'Terlambat') {
-            ketBadgeMasuk = '<span class=badge-danger" style="font-size: 10px; background-color: #bb2124; color: white; padding: 5px 10px; border-radius: 5px;">Masuk Terlambat</span>';
-          } else if (ketMasuk === 'Sakit') {
-            ketBadgeMasuk = '<span class="badge-warning" style="font-size: 10px; background-color: #ffcc00; color: white; padding: 5px 10px; border-radius: 5px;">Masuk Sakit</span>';
-          } else if (ketMasuk === 'izin') {
-            ketBadgeMasuk = '<span class=badge-warning" style="font-size: 10px; background-color: #ff8700; color: white; padding: 5px 10px; border-radius: 5px;">Masuk Izin</span>'
-          } else {
-            ketBadgeMasuk = "<span>Belum Presensi Masuk</span>";
-          }
+          // Search Functionality
+          const searchInput = CihuyId("searchInput");
+          const searchButton = CihuyId("searchButton");
 
-          // Pengkondisian Badge Keterangan
-          let ketBadgePulang = '';
-          if (ketPulang === 'Lebih Cepat') {
-            ketBadgePulang = '<span class="badge-green" style="font-size: 10px; background-color: #ff8700; color: white; padding: 5px 10px; border-radius: 5px;">Pulang Lebih Cepat</span>';
-          } else if (ketPulang === 'Tepat Waktu') {
-            ketBadgePulang = '<span class="badge-blue" style="font-size: 10px; background-color: #0d6efd; color: white; padding: 5px 10px; border-radius: 5px;">Pulang Tepat Waktu</span>';
-          } else if (ketPulang === 'Lebih Lama') {
-            ketBadgePulang = '<span class=badge-danger" style="font-size: 10px; background-color: #22bb33; color: white; padding: 5px 10px; border-radius: 5px;">Pulang Lebih Lama</span>';
-          } else {
-            ketBadgePulang = "<span>Belum Presensi Pulang</span>";
-          }
+          searchButton.addEventListener("click", () => {
+            const searchTerm = searchInput.value.toLowerCase();
+            if (searchTerm) {
+              const searchData = combinedData.filter((entry) => {
+                const namaLowerCase = entry.masuk.biodata.nama.toLowerCase();
+                return namaLowerCase.includes(searchTerm);
+              });
 
-          let statusKerja = '';
-          if (persentaseStatus >= 100) {
-            statusKerja = '<span class=badge-danger" style="font-size: 10px; background-color: #22bb33; color: white; padding: 5px 10px; border-radius: 5px;">Tuntas</span>'
-          } else if (persentaseStatus < 100) {
-            statusKerja = '<span class=badge-danger" style="font-size: 10px; background-color: #bb2124; color: white; padding: 5px 10px; border-radius: 5px;">Belum Tuntas</span>';
-          } else {
-            statusKerja = '';
-          }
+              filteredData = searchData;
 
-        tableData += `
-        <tr>
-          <td>
-              <div class="d-flex align-items-center">
-                    <div class="ms-3">
-                        <p class="fw-bold mb-1">${nama}</p>
-                        <p class="text-muted mb-0">${masukEntry.phone_number}</p>
-                    </div>
-                </div>
-            </td>
-            <td>
-                <p class="fw-normal mb-1">${masukEntry.biodata.jabatan}</p>
-            </td>
-            <td style="text-align: center; vertical-align: middle">
-                <p class="fw-normal mb-1"><b>${ketBadgeMasuk}</b> ${jamMasuk}</p>
-                <p class="fw-normal mb-1"><b>${ketBadgePulang}</b> ${jamPulang}</p>
-            </td>
-            <td style="text-align: center; vertical-align: middle">
-                <p class="fw-normal mb-1">${date}</p>
-            </td>
-            <td style="text-align: center; vertical-align: middle">
-                <p class="fw-normal mb-1">${Durasi}</p>
-            </td>
-            <td style="text-align: center; vertical-align: middle">
-                <p class="fw-normal mb-1">${Persentase}</p>
-            </td>
-            <td style="text-align: center; vertical-align: middle">
-                <p class="fw-normal mb-1">${statusKerja}</p>
-            </td>
-            <td style="text-align: center; vertical-align: middle">
-                <p class="fw-normal mb-1">${lampiranContent}</p>
-            </td>
-        </tr>
-        `
-      })
-      document.getElementById("tablebody").innerHTML = tableData;
-
-      // // Call the calculateAndSetAverage function to calculate and update the average on page load
-      // calculateAndSetAverage();
-      
-      // Untuk Memunculkan Pagination Halamannya
-      displayData(halamannow);
-			updatePagination();
-
-    })
-    .catch(error => {
-      console.log('error', error);
+              totalData = filteredData.length;
+              halamannow = 1;
+              displayData(halamannow);
+              updatePagination();
+            } else {
+              // If the search term is empty, display the original data
+              filteredData = combinedData;
+              totalData = combinedData.length;
+              halamannow = 1;
+              displayData(halamannow);
+              updatePagination();
+            }
+          });
+        })
+        .catch(error => {
+          console.log('error', error);
+        });
     });
 
   // Fungsi Untuk Menampilkan Data
-	function displayData(page) {
-		const baris = CihuyQuerySelector("#tablebody tr");
-		const mulaiindex = (page - 1) * itemperpage;
-		const akhirindex = mulaiindex + itemperpage;
+  function displayData(page) {
+    const mulaiindex = (page - 1) * itemperpage;
+    const akhirindex = mulaiindex + itemperpage;
+    const rowsToShow = filteredData.slice(mulaiindex, akhirindex);
 
-		for (let i = 0; i < baris.length; i++) {
-			if (i >= mulaiindex && i < akhirindex) {
-				baris[i].style.display = "table-row";
-			} else {
-				baris[i].style.display = "none";
-			}
-		}
-	}
+    let tableData = "";
+
+    // Iterasi melalui rowsToShow dan membangun baris tabel
+    rowsToShow.forEach((combinedEntry) => {
+      const masukEntry = combinedEntry.masuk;
+      const pulangEntry = combinedEntry.pulang;
+
+      // Ekstrak data yang relevan
+      const nama = masukEntry.biodata.nama;
+      const lampiran = masukEntry.lampiran;
+      const ketMasuk = masukEntry?.ket;
+      const ketPulang = pulangEntry?.ket;
+      const date = new Date(masukEntry.Datetime).toLocaleDateString();
+      const jamMasuk = new Date(masukEntry.Datetime).toLocaleTimeString();
+      const jamPulang = pulangEntry ? new Date(pulangEntry.datetime).toLocaleTimeString() : '';
+      const Durasi = pulangEntry ? pulangEntry.durasi : '0 Jam 0 Menit 0 Detik';
+      const persentaseStatus = pulangEntry ? parseFloat(pulangEntry.persentase) : 0;
+      const Persentase = pulangEntry ? pulangEntry.persentase : '0%';
+      const lampiranContent = lampiran ? lampiran : '<p>Tidak Ada Catatan</p>';
+
+      // Pengkondisian Badge Keterangan
+      let ketBadgeMasuk = getBadgeMarkup(ketMasuk);
+      let ketBadgePulang = getBadgeMarkup(ketPulang);
+
+      let statusKerja = getStatusBadgeMarkup(persentaseStatus);
+
+      tableData += `
+          <tr>
+              <td>
+                  <div class="d-flex align-items-center">
+                      <div class="ms-3">
+                          <p class="fw-bold mb-1">${nama}</p>
+                          <p class="text-muted mb-0">${masukEntry.phone_number}</p>
+                      </div>
+                  </div>
+              </td>
+              <td>
+                  <p class="fw-normal mb-1">${masukEntry.biodata.jabatan}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                  <p class="fw-normal mb-1"><b>${ketBadgeMasuk}</b> ${jamMasuk}</p>
+                  <p class="fw-normal mb-1"><b>${ketBadgePulang}</b> ${jamPulang}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                  <p class="fw-normal mb-1">${date}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                  <p class="fw-normal mb-1">${Durasi}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                  <p class="fw-normal mb-1">${Persentase}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                  <p class="fw-normal mb-1">${statusKerja}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                  <p class="fw-normal mb-1">${lampiranContent}</p>
+              </td>
+          </tr>
+      `;
+    });
+
+    document.getElementById("tablebody").innerHTML = tableData;
+  }
 
   // Fungsi Untuk Update Pagination
-	function updatePagination() {
-		halamansaatini.textContent = `Halaman ${halamannow}`;
-	}
+  function updatePagination() {
+    halamansaatini.textContent = `Halaman ${halamannow}`;
+  }
 
-  // Button Pagination (Sebelumnya)
-	buttonsebelumnya.addEventListener("click", () => {
-		if (halamannow > 1) {
-			halamannow--;
-			displayData(halamannow);
-			updatePagination();
-		}
-	});
+  // Function to get badge markup based on the status
+  function getBadgeMarkup(status) {
+    switch (status) {
+      case 'Lebih Cepat':
+        return '<span class="badge-green" style="font-size: 10px; background-color: #22bb33; color: white; padding: 5px 10px; border-radius: 5px;">Lebih Cepat</span>';
+      case 'Tepat Waktu':
+        return '<span class="badge-blue" style="font-size: 10px; background-color: #0d6efd; color: white; padding: 5px 10px; border-radius: 5px;">Tepat Waktu</span>';
+      case 'Terlambat':
+        return '<span class="badge-danger" style="font-size: 10px; background-color: #bb2124; color: white; padding: 5px 10px; border-radius: 5px;">Terlambat</span>';
+      case 'Sakit':
+        return '<span class="badge-warning" style="font-size: 10px; background-color: #ffcc00; color: white; padding: 5px 10px; border-radius: 5px;">Sakit</span>';
+      case 'izin':
+        return '<span class="badge-warning" style="font-size: 10px; background-color: #ff8700; color: white; padding: 5px 10px; border-radius: 5px;">Izin</span>';
+      default:
+        return "<span>Belum Presensi</span>";
+    }
+  }
 
-  // Button Pagination (Selanjutnya)
-	buttonselanjutnya.addEventListener("click", () => {
-		const totalPages = Math.ceil(
-			tablebody.querySelectorAll("#tablebody tr").length / itemperpage
-		);
-		if (halamannow < totalPages) {
-			halamannow++;
-			displayData(halamannow);
-			updatePagination();
-		}
-	});
-  })
+  // Function to get badge markup based on the status
+  function getStatusBadgeMarkup(persentaseStatus) {
+    if (persentaseStatus >= 100) {
+      return '<span class="badge-danger" style="font-size: 10px; background-color: #22bb33; color: white; padding: 5px 10px; border-radius: 5px;">Tuntas</span>';
+    } else if (persentaseStatus < 100) {
+      return '<span class="badge-danger" style="font-size: 10px; background-color: #bb2124; color: white; padding: 5px 10px; border-radius: 5px;">Belum Tuntas</span>';
+    } else {
+      return '';
+    }
+  }
 });

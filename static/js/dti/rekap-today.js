b953dabcb8cd7f21092c8a540cfb383df8ff5744
rofinafiin <rofinafiisr@gmail.com>
2024-01-26 15:11:59 +0700

    page rekapitulasi commit 24 jam kebelakang

---
+++ static/js/dti/rekap-today.js
@@ -0,0 +1,114 @@
+import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
+import { CihuyDomReady } from "https://c-craftjs.github.io/table/table.js";
+import { getBadgeMarkup, getBadgeCommit } from "../style/badge.js"
+
+// Untuk Autentifikasi Login User Tertentu
+import { token } from "../controller/cookies.js";
+
+var header = new Headers();
+header.append("login", token);
+header.append("Content-Type", "application/json");
+
+const requestOptions = {
+  method: "GET",
+  headers: header
+};
+
+
+// Untuk Get All data Presensi
+CihuyDomReady(() => {
+//   const tablebody = CihuyId("tablebody");
+  const buttonsebelumnya = CihuyId("prevPageBtn");
+  const buttonselanjutnya = CihuyId("nextPageBtn");
+  const halamansaatini = CihuyId("currentPage");
+  const itemperpage = 10;
+  let halamannow = 1;
+  let filteredData = []; // To store the filtered data for search
+  let totalData = 0;
+
+  // Ambil data masuk
+  fetch("https://hris_backend.ulbi.ac.id/api/v2/commits/24jam", requestOptions)
+    .then((result) => result.json())
+    .then((rekapharian) => {
+        let rkp = rekapharian.data
+          // Sortir array combinedData berdasarkan tanggal masuk
+          rkp.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+            console.log(rkp)
+          // Inisialisasi data tabel
+          totalData = rkp.length;
+          filteredData = rkp;
+
+          // Untuk Memunculkan Pagination Halamannya
+          displayData(halamannow);
+          updatePagination();
+
+          // Button Pagination (Sebelumnya)
+          buttonsebelumnya.addEventListener("click", () => {
+            if (halamannow > 1) {
+              halamannow--;
+              displayData(halamannow);
+              updatePagination();
+            }
+          });
+
+          // Button Pagination (Selanjutnya)
+          buttonselanjutnya.addEventListener("click", () => {
+            const totalPages = Math.ceil(totalData / itemperpage);
+            if (halamannow < totalPages) {
+              halamannow++;
+              displayData(halamannow);
+              updatePagination();
+            }
+          })
+    });
+
+  // Fungsi Untuk Menampilkan Data
+  function displayData(page) {
+    const mulaiindex = (page - 1) * itemperpage;
+    const akhirindex = mulaiindex + itemperpage;
+    const rowsToShow = filteredData.slice(mulaiindex, akhirindex);
+
+    let tableData = "";
+
+    // Iterasi melalui rowsToShow dan membangun baris tabel
+    rowsToShow.forEach((combinedEntry) => {
+
+      // Ekstrak data yang relevan
+      const username = combinedEntry.username;
+      const email = combinedEntry.email;
+      const repository = combinedEntry.repo;
+      const message = combinedEntry.message;
+      const ref = combinedEntry.ref;
+
+      tableData += `
+          <tr>
+              <td>
+                  <div class="d-flex align-items-center">
+                      <div class="ms-3">
+                          <p class="fw-bold mb-1">${username}</p>
+                          <p class="text-muted mb-0">${email}</p>
+                      </div>
+                  </div>
+              </td>
+              <td>
+              <p class="fw-normal mb-1">${repository}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+              <p class="fw-normal mb-1"><b>${message}</b></p>
+              </td>
+              <td>
+                  <p class="fw-normal mb-1">${ref}</p>
+              </td>
+          </tr>
+      `;
+    });
+
+    document.getElementById("tablebody").innerHTML = tableData;
+  }
+
+  // Fungsi Untuk Update Pagination
+  function updatePagination() {
+    halamansaatini.textContent = `Halaman ${halamannow}`;
+  }
+});

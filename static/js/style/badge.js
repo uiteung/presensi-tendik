30f17c73378a713c9b64a69b8d8ac25e2dc48b74
rofinafiin <rofinafiisr@gmail.com>
2024-01-29 15:26:15 +0700

    get badge pulanf

--- static/js/style/badge.js
+++ static/js/style/badge.js
@@ -17,6 +17,24 @@ export function getBadgeMarkup(status) {
 }
 
 // Function to get badge markup based on the status
+export function getBadgePulang(status) {
+  switch (status) {
+    case 'Lebih Cepat':
+      return '<span class="badge-green" style="font-size: 10px; background-color: #22bb33; color: white; padding: 5px 10px; border-radius: 5px;">Lebih Cepat</span>';
+    case 'Tepat Waktu':
+      return '<span class="badge-blue" style="font-size: 10px; background-color: #0d6efd; color: white; padding: 5px 10px; border-radius: 5px;">Tepat Waktu</span>';
+    case 'Lebih Lama':
+      return '<span class="badge-danger" style="font-size: 10px; background-color: #bb2124; color: white; padding: 5px 10px; border-radius: 5px;">Lebih Lama</span>';
+    case 'Sakit':
+      return '<span class="badge-warning" style="font-size: 10px; background-color: #ffcc00; color: white; padding: 5px 10px; border-radius: 5px;">Sakit</span>';
+    case 'izin':
+      return '<span class="badge-warning" style="font-size: 10px; background-color: #ff8700; color: white; padding: 5px 10px; border-radius: 5px;">Izin</span>';
+    default:
+      return "<span>Belum Presensi</span>";
+  }
+}
+
+// Function to get badge markup based on the status
 export function getStatusBadgeMarkup(persentaseStatus) {
     if (persentaseStatus >= 100) {
       return '<span class="badge-danger" style="font-size: 10px; background-color: #22bb33; color: white; padding: 5px 10px; border-radius: 5px;">Tuntas</span>';

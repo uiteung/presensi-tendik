efe9adad08d64022aa1f48a79bf35002a2eb9bf7
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-30 10:14:38 +0700

    add endpoint UrlGetAllMasukHarian & UrlGetAllPulangHarian

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -1,4 +1,6 @@
 // Endpoint Get All
+export let UrlGetAllMasukHarian = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/masukharian";
+export let UrlGetAllPulangHarian = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/pulangharian";
 
 // Endpoint Post
 export let UrlPostDataKaryawan = 'https://hris_backend.ulbi.ac.id/presensi/datakaryawan/postdata';

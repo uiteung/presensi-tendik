f76c9a9e18c4f42aff0babc1b2beca679d4b32d1
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-30 10:17:47 +0700

    add endpoint UrlGetKaryawanById & UrlPutDataKaryawan

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -2,5 +2,11 @@
 export let UrlGetAllMasukHarian = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/masukharian";
 export let UrlGetAllPulangHarian = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/pulangharian";
 
+// Endpoint Get By Id
+export let UrlGetKaryawanById = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/getkaryawan/";
+
 // Endpoint Post
-export let UrlPostDataKaryawan = 'https://hris_backend.ulbi.ac.id/presensi/datakaryawan/postdata';
+export let UrlPostDataKaryawan = "https://hris_backend.ulbi.ac.id/presensi/datakaryawan/postdata";
+
+// Endpoint Update
+export let UrlPutDataKaryawan = "https://hris_backend.ulbi.ac.id/presensi/datakaryawan/updatedata/";

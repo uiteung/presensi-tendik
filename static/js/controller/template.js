f2c45bf2ab8388de2d50234fd588b14a739ce85c
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-01-30 10:20:08 +0700

    add endpoint UrlGetAllPresensi & UrlGetAllPresensiPulang

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -1,6 +1,8 @@
 // Endpoint Get All
 export let UrlGetAllMasukHarian = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/masukharian";
 export let UrlGetAllPulangHarian = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/pulangharian";
+export let UrlGetAllPresensi = "https://hris_backend.ulbi.ac.id/presensi/datapresensi";
+export let UrlGetAllPresensiPulang = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/pulang";
 
 // Endpoint Get By Id
 export let UrlGetKaryawanById = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/getkaryawan/";

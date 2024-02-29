06f23087ec8352bced01791cf36f1620873ecf1e
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-29 14:13:41 +0700

    create content render sidebar

---
+++ static/theme/bar/sidebar.js
@@ -0,0 +1,4 @@
+// Untuk render sidebar
+document.addEventListener('DOMContentLoaded', function() {
+    $("#sidebar").load("https://euis.ulbi.ac.id/presensi-tendik/static/theme/sidebar.html");
+});

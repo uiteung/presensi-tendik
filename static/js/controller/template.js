d15c4dde16551e7a4c43e4155c59c7dfd2f1bee6
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-02-29 14:48:55 +0700

    add const request option for method CRUD

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -1,3 +1,16 @@
+import { token } from "./cookies.js";
+
+// Create Header
+var header = new Headers();
+header.append("login", token);
+header.append("Content-Type", "application/json");
+
+// Request Option
+export let requestOptionsGet = { method: "GET", headers: header };
+export let requestOptionsDelete = { method: "DELETE", headers: header };
+export let requestOptionsPost = { method: "POST", headers: header };
+export let requestOptionsUpdate = { method: "PATCH", headers: header };
+
 // Endpoint Get All
 export let UrlGetAllMasukHarian = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/masukharian";
 export let UrlGetAllPulangHarian = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/pulangharian";
@@ -5,6 +18,7 @@ export let UrlGetAllPresensi = "https://hris_backend.ulbi.ac.id/presensi/datapre
 export let UrlGetAllPresensiPulang = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/pulang";
 export let UrlGetAllCommitsRekapHarian = "https://hris_backend.ulbi.ac.id/api/v2/commits/rekapharian";
 export let UrlGetAllPerforma = "https://hris_backend.ulbi.ac.id/api/v2/commits/performance";
+export let UrlGetAllKaryawan = "https://hris_backend.ulbi.ac.id/presensi/datakaryawan";
 
 // Endpoint Get By Id
 export let UrlGetKaryawanById = "https://hris_backend.ulbi.ac.id/presensi/datapresensi/getkaryawan";
@@ -14,3 +28,6 @@ export let UrlPostDataKaryawan = "https://hris_backend.ulbi.ac.id/presensi/datak
 
 // Endpoint Update
 export let UrlPutDataKaryawan = "https://hris_backend.ulbi.ac.id/presensi/datakaryawan/updatedata/";
+
+// Endpoint Delete
+export let UrlDeleteKaryawan = "https://hris_backend.ulbi.ac.id/presensi/datakaryawan/deletedata";

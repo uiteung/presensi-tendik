f2cb8eee6c2c458c4d5cdf0c6aed3e0f55fa72f6
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-04 14:23:43 +0700

    create function fetch

---
+++ static/js/dti/rekap-layanan.js
@@ -0,0 +1,81 @@
+import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
+import { CihuyDomReady, CihuyQuerySelector } from "https://c-craftjs.github.io/table/table.js";
+
+// Untuk Membuat Pagination
+CihuyDomReady(() => {
+	const tablebody = CihuyId("tablebody");
+	const buttonsebelumnya = CihuyId("prevPageBtn");
+	const buttonselanjutnya = CihuyId("nextPageBtn");
+	const halamansaatini = CihuyId("currentPage");
+	const itemperpage = 5;
+	let halamannow = 1;
+
+fetch(AllBank, requestOptionsGet)
+	.then((result) => {
+		return result.json();
+	})
+	.then((data) => {
+		let tableData = "";
+		data.data.map((values) => {
+			tableData += `
+                        <tr>
+                        <td hidden></td>
+                        <td style="text-align: center; vertical-align: middle">
+                            <p class="fw-normal mb-1">${values.name_rek}</p>
+                        </td>
+                        <td style="text-align: center; vertical-align: middle">
+                            <p class="fw-normal mb-1">${values.no_rek}</p>
+                        </td>
+                        <td style="text-align: center; vertical-align: middle">
+                            <p class="fw-normal mb-1">${values.bank}</p>
+                        </td>
+                        <td style="text-align: center; vertical-align: middle">
+                            <p class="fw-normal mb-1">${getBadgeBank(values.status)}</p>
+                        </td>
+                    </tr>`;
+		});
+		document.getElementById("tablebody").innerHTML = tableData;
+
+		displayData(halamannow);
+		updatePagination();		
+	})
+	.catch(error => {
+		console.log('error', error);
+	});
+
+function displayData(page) {
+	const baris = CihuyQuerySelector("#tablebody tr");
+	const mulaiindex = (page - 1) * itemperpage;
+	const akhirindex = mulaiindex + itemperpage;
+
+	for (let i = 0; i < baris.length; i++) {
+		if (i >= mulaiindex && i < akhirindex) {
+			baris[i].style.display = "table-row";
+		} else {
+			baris[i].style.display = "none";
+		}
+	}
+}
+function updatePagination() {
+	halamansaatini.textContent = `Halaman ${halamannow}`;
+}
+
+buttonsebelumnya.addEventListener("click", () => {
+	if (halamannow > 1) {
+		halamannow--;
+		displayData(halamannow);
+		updatePagination();
+	}
+});
+
+buttonselanjutnya.addEventListener("click", () => {
+	const totalPages = Math.ceil(
+		tablebody.querySelectorAll("#tablebody tr").length / itemperpage
+	);
+	if (halamannow < totalPages) {
+		halamannow++;
+		displayData(halamannow);
+		updatePagination();
+	}
+  });
+});
